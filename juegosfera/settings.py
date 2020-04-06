951fed68da683e5897255e824301730ba624cc0a
Juan Manuel Corredor Roldan <jumcorredorro@unal.edu.co>
2020-04-06 08:47:02 -0500

    cors

--- juegosfera/settings.py
+++ juegosfera/settings.py
@@ -40,18 +40,22 @@ INSTALLED_APPS = [
     'apps.index',
     'rest_framework',
     'rest_framework.authtoken',
-    'api'
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'juegosfera.urls'
 
